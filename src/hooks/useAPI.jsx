5dcf5b5fe13ec7b35d06f6cc26bd18e5ae335944
Steven Rothenburger <steven.rothenburger@vizworx.com>
2021-02-17 11:42:07 -0700

    fix(useAPI): comment out useAPI and fix lint errors
    comments out useAPI so we can reimplement it later, but it no longer errors

--- src/hooks/useAPI.jsx
+++ src/hooks/useAPI.jsx
@@ -1,16 +1,17 @@
-import { useMemo } from 'react';
-import { useQuery } from '@apollo/react-hooks';
+// import { useMemo } from 'react';
+// import { useQuery } from '@apollo/react-hooks';
 
-import getI18NMessages from '../utilities/getI18NMessages';
-import { ITERATIONS_TRANSLATIONS } from './queries';
+// import getI18NMessages from '../utilities/getI18NMessages';
+// import { ITERATIONS_TRANSLATIONS } from './queries';
 
-export default () => {
-  const { loading, error, data } = useQuery(ITERATIONS_TRANSLATIONS);
+export default () => null;
 
-  const translations = useMemo(
-    () => (data ? getI18NMessages(data.translations) : {}),
-    [data],
-  );
+// const { loading, error, data } = useQuery(ITERATIONS_TRANSLATIONS);
 
-  return { loading, error, translations };
-};
+// const translations = useMemo(
+//   () => (data ? getI18NMessages(data.translations) : {}),
+//   [data],
+// );
+
+// return { loading, error, translations };
+
