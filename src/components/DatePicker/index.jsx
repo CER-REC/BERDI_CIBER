a918fba124098e85d649ad131f1a5ba165a9d1ed
Steven Rothenburger <steven.rothenburger@vizworx.com>
2021-02-19 14:27:27 -0700

    feat(datePicker): adds new range date picker

--- src/components/DatePicker/index.jsx
+++ src/components/DatePicker/index.jsx
@@ -1,11 +1,12 @@
 import 'date-fns';
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
+import DatePicker from 'react-datepicker';
 import { makeStyles, createStyles, InputLabel, FormControl } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => createStyles({
@@ -48,31 +49,52 @@ const useStyles = makeStyles((theme) => createStyles({
 
 export default function MaterialUIPickers() {
   // The first commit of Material-UI
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  // const [selectedDate, setSelectedDate] = React.useState(new Date());
 
   const classes = useStyles();
 
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
+  // const handleDateChange = (date) => {
+  //   setSelectedDate(date);
+  // };
+
+  const [startDate, setStartDate] = useState(new Date('2014/02/08'));
+  const [endDate, setEndDate] = useState(new Date('2014/02/10'));
 
   return (
-    <FormControl className={classes.margin}>
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <InputLabel className={classes.label}>Date</InputLabel>
-        <KeyboardDatePicker
-          className={classes.datePicker}
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-      </MuiPickersUtilsProvider>
-    </FormControl>
+  // <FormControl className={classes.margin}>
+  //   <MuiPickersUtilsProvider utils={DateFnsUtils}>
+  //     <InputLabel className={classes.label}>Date</InputLabel>
+  //     <KeyboardDatePicker
+  //       className={classes.datePicker}
+  //       disableToolbar
+  //       variant="inline"
+  //       format="MM/dd/yyyy"
+  //       margin="normal"
+  //       value={selectedDate}
+  //       onChange={handleDateChange}
+  //       KeyboardButtonProps={{
+  //         'aria-label': 'change date',
+  //       }}
+  //     />
+  //   </MuiPickersUtilsProvider>
+  // </FormControl>
+
+    <>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        selectsStart
+        startDate={startDate}
+        endDate={endDate}
+      />
+      <DatePicker
+        selected={endDate}
+        onChange={(date) => setEndDate(date)}
+        selectsEnd
+        startDate={startDate}
+        endDate={endDate}
+        minDate={startDate}
+      />
+    </>
   );
 }
