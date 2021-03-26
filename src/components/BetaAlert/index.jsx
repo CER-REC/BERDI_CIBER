57978a11459230221dbdd689903a7a82cf64e6bd
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2021-03-26 16:31:03 -0600

    feat: Updated link in beta component and added the GTM event
    1282

--- src/components/BetaAlert/index.jsx
+++ src/components/BetaAlert/index.jsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Button, makeStyles } from '@material-ui/core';
 import { useIntl } from 'react-intl';
 
+import useConfig from '../../hooks/useConfig';
+import { reportSection } from '../../utilities/analytics';
+
+const useStyles = makeStyles(() => ({
+  link: {
+    fontSize: 16,
+    padding: 0,
+    lineHeight: 'normal',
+    textDecoration: 'underline',
+    textTransform: 'none',
+    verticalAlign: 'text-bottom',
+    '&:hover': {
+      backgroundColor: 'inherit',
+    },
+  },
+}));
+
 const BetaAlert = () => {
+  const classes = useStyles();
   const intl = useIntl();
+  const { configDispatch } = useConfig();
+  const handleClick = useCallback(() => {
+    reportSection('project');
+    configDispatch({ type: 'page/changed', payload: 'project' });
+  }, [configDispatch]);
 
   return (
-    <div className="alert alert-info">
+    <div className="BetaAlert alert alert-info">
       <span>
         <strong>{intl.formatMessage({ id: 'pages.landing.alert.title' })}</strong>
         &nbsp;-&nbsp;
         {intl.formatMessage({ id: 'pages.landing.alert.body' })}
         &nbsp;
-        <a href="https://www.cer-rec.gc.ca/">{intl.formatMessage({ id: 'pages.landing.alert.link' })}</a>
+        <Button
+          classes={{ text: classes.link }}
+          variant="text"
+          onClick={handleClick}
+        >
+          {intl.formatMessage({ id: 'pages.landing.alert.link' })}
+        </Button>
       </span>
     </div>
   );
