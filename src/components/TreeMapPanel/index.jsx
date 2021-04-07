b6fb35361f3355ee659e2884ff0d16a20724788c
Steven Rothenburger <steven.rothenburger@vizworx.com>
2021-04-07 12:01:41 -0700

    feat(treeMap): adds a no results message when there is no data
    1308

--- src/components/TreeMapPanel/index.jsx
+++ src/components/TreeMapPanel/index.jsx
@@ -20,6 +20,8 @@ const TreeMapPanel = () => {
   const [open, setOpen] = useState(false);
   const [selectedBoxData, setSelectedBoxData] = useState(null);
 
+  const combinedSearches = config.searches.reduce((acc, val) => (`${acc} `).concat(val), '');
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -28,81 +30,89 @@ const TreeMapPanel = () => {
     setOpen(false);
   };
 
-  if (!data) {
-    return null;
-  }
-
   return (
     <>
       <Typography variant="h6" className={classes.countsTitle}>
         {intl.formatMessage({ id: 'components.treeMap.title' })}
       </Typography>
-      <Typography variant="h6" className={classes.countsText}>
-        {intl.formatMessage({ id: 'components.treeMap.countsText' }, {
-          tables: data.tableCount,
-          figures: data.figureCount,
-        })}
-      </Typography>
-      <Typography style={{ padding: '0.5em 0 0.2em 0' }}>
-        {intl.formatMessage({ id: 'components.treeMap.boxSelectText' })}
-      </Typography>
-      <TreeMapDialog open={open} handleClose={handleClose} leafData={selectedBoxData} />
 
-      <Grid className={classes.treeMap}>
-        <ResponsiveTreeMapHtml
-          tooltip={(application) => (
-            <div className={classes.tooltip}>
-              <p>{ application.data.shortName }</p>
-              <p>
-                <strong>{application.data.tableCount}</strong>
-                {` ${intl.formatMessage({ id: 'common.tables' })}`}
-              </p>
-              <p>
-                <strong>{application.data.figureCount}</strong>
-                {` ${intl.formatMessage({ id: 'common.figures' })}`}
-              </p>
-            </div>
-          )}
-          root={data}
-          identity="shortName"
-          value="totalCount"
-          valueFormat=".02s"
-          orientLabel={false}
-          tile="squarify"
-          labelSkipSize={0}
-          labelTextColor="black"
-          borderWidth={3}
-          borderColor="#ffffff"
-          colors={(d) => d.color}
-          onClick={
-            (node) => {
-              reportProject(
-                config.regions,
-                config.commodities,
-                config.projectTypes,
-                config.statuses,
-                node.data.name,
-              );
-              setSelectedBoxData(node.data);
-              handleClickOpen();
-            }
-          }
-          label={(d) => (
-            <>
-              <div className={classes.emptyPlaceholder} />
-              <div className={classes.labelInner}>
-                <span className={classes.labelInnerTitle}>{d.shortName}</span>
-              </div>
-              <div className={classes.labelInner}>
-                <span className={classes.labelInnerCounts}>{`${d.tableCount} ${intl.formatMessage({ id: 'common.tables' })}`}</span>
-              </div>
-              <div className={classes.labelInner} style={{ paddingTop: '0' }}>
-                <span className={classes.labelInnerCounts}>{`${d.figureCount} ${intl.formatMessage({ id: 'common.figures' })}`}</span>
-              </div>
-            </>
-          )}
-        />
-      </Grid>
+      {!data
+        ? (
+          <Typography variant="h6" style={{ fontWeight: '200' }}>
+            {intl.formatMessage({ id: 'components.treeMap.noResultsText' }, {
+              searches: (<strong style={{ fontWeight: '700' }}>{combinedSearches}</strong>),
+            })}
+          </Typography>
+        )
+        : (
+          <>
+            <Typography variant="h6" className={classes.countsText}>
+              {intl.formatMessage({ id: 'components.treeMap.countsText' }, {
+                tables: data.tableCount,
+                figures: data.figureCount,
+              })}
+            </Typography>
+            <Typography style={{ padding: '0.5em 0 0.2em 0' }}>
+              {intl.formatMessage({ id: 'components.treeMap.boxSelectText' })}
+            </Typography>
+
+            <TreeMapDialog open={open} handleClose={handleClose} leafData={selectedBoxData} />
+
+            <Grid className={classes.treeMap}>
+              <ResponsiveTreeMapHtml
+                tooltip={(application) => (
+                  <div className={classes.tooltip}>
+                    <p>{ application.data.shortName }</p>
+                    <p>
+                      <strong>{application.data.tableCount}</strong>
+                      {` ${intl.formatMessage({ id: 'common.tables' })}`}
+                    </p>
+                    <p>
+                      <strong>{application.data.figureCount}</strong>
+                      {` ${intl.formatMessage({ id: 'common.figures' })}`}
+                    </p>
+                  </div>
+                )}
+                root={data}
+                identity="shortName"
+                value="totalCount"
+                valueFormat=".02s"
+                orientLabel={false}
+                tile="squarify"
+                labelSkipSize={0}
+                labelTextColor="black"
+                borderWidth={3}
+                borderColor="#ffffff"
+                colors={(d) => d.color}
+                onClick={(node) => {
+                  reportProject(
+                    config.regions,
+                    config.commodities,
+                    config.projectTypes,
+                    config.statuses,
+                    node.data.name,
+                  );
+                  setSelectedBoxData(node.data);
+                  handleClickOpen();
+                }}
+                label={(d) => (
+                  <>
+                    <div className={classes.emptyPlaceholder} />
+                    <div className={classes.labelInner}>
+                      <span className={classes.labelInnerTitle}>{d.shortName}</span>
+                    </div>
+                    <div className={classes.labelInner}>
+                      <span className={classes.labelInnerCounts}>{`${d.tableCount} ${intl.formatMessage({ id: 'common.tables' })}`}</span>
+                    </div>
+                    <div className={classes.labelInner} style={{ paddingTop: '0' }}>
+                      <span className={classes.labelInnerCounts}>{`${d.figureCount} ${intl.formatMessage({ id: 'common.figures' })}`}</span>
+                    </div>
+                  </>
+                )}
+              />
+            </Grid>
+          </>
+        ) }
     </>
   );
 };
