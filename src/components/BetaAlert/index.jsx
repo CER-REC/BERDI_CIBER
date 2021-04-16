b410044ee63b33702ababed4cf916662e22de5a4
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2021-04-16 11:34:53 -0600

    refactor: Extracted common alert component from beta and accuracy alerts
    1458

--- src/components/BetaAlert/index.jsx
+++ src/components/BetaAlert/index.jsx
@@ -1,38 +1,29 @@
-import React, { useCallback } from 'react';
-import { Button } from '@material-ui/core';
+import React from 'react';
 import { useIntl } from 'react-intl';
 
 import useConfig from '../../hooks/useConfig';
 import { reportSection } from '../../utilities/analytics';
+import Alert from '../Alert';
 
 const BetaAlert = () => {
   const intl = useIntl();
   const { configDispatch } = useConfig();
-  const handleClick = useCallback(() => {
+  const handleClick = () => {
     reportSection('project');
     configDispatch({
       type: 'page/fragment/changed',
       payload: { page: 'project', fragment: 'learn' },
     });
-  }, [configDispatch]);
+  };
 
   return (
-    <div className="BetaAlert alert alert-info">
-      <span>
-        &nbsp;
-        <strong>{intl.formatMessage({ id: 'pages.landing.alert.title' })}</strong>
-        &nbsp;-&nbsp;
-        {intl.formatMessage({ id: 'pages.landing.alert.body' })}
-        &nbsp;
-        <Button
-          color="inherit"
-          onClick={handleClick}
-          disableRipple
-        >
-          {intl.formatMessage({ id: 'pages.landing.alert.link' })}
-        </Button>
-      </span>
-    </div>
+    <Alert
+      className="BetaAlert alert-info"
+      title={intl.formatMessage({ id: 'components.betaAlert.title' })}
+      body={intl.formatMessage({ id: 'components.betaAlert.body' })}
+      link={intl.formatMessage({ id: 'components.betaAlert.link' })}
+      onClick={handleClick}
+    />
   );
 };
 
