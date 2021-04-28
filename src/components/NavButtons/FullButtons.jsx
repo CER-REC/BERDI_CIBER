c4d0dd7efa74ac4a286a39acc598535014ee38eb
Steven Rothenburger <steven.rothenburger@vizworx.com>
2021-04-28 10:43:08 -0700

    style(FullButtons): removes unused import

--- src/components/NavButtons/FullButtons.jsx
+++ src/components/NavButtons/FullButtons.jsx
@@ -1,4 +1,4 @@
-import { Grid, makeStyles, Typography } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import React, { useCallback } from 'react';
 import { useIntl } from 'react-intl';
 import useConfig from '../../hooks/useConfig';
