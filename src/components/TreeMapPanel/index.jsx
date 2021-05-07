88c4e8b1cc7eeddd8b4ef72b102ee0103f0c9fbc
Austin Sullivan <austin.sullivan@vizworx.com>
2021-05-07 14:53:39 -0600

    feat(TreeMapPanel/index): slimmed down TreeMapPanel component code now that it is using NoResultsSta
    Issues Affected: BUG 2842

--- src/components/TreeMapPanel/index.jsx
+++ src/components/TreeMapPanel/index.jsx
@@ -2,10 +2,9 @@ import React from 'react';
 import { Typography, makeStyles } from '@material-ui/core';
 import { useIntl } from 'react-intl';
 
-import useConfig from '../../hooks/useConfig';
 import useESAData from '../../hooks/useESAData';
 import TreeMap from './TreeMap';
-import useAPI from '../../hooks/useAPI';
+import NoResultsStatusMessages from './NoResultsStatusMessages';
 
 const useStyles = makeStyles({
   title: {
@@ -23,10 +22,7 @@ const useStyles = makeStyles({
 const TreeMapPanel = () => {
   const intl = useIntl();
   const classes = useStyles();
-  const { config } = useConfig();
-  const { minDate, maxDate } = useAPI();
   const { applications, loading } = useESAData();
-  const combinedSearches = config.searches.join(' ');
   const figureCount = applications.reduce(
     (count, application) => (application.figureCount + count),
     0,
@@ -40,17 +36,6 @@ const TreeMapPanel = () => {
     return null;
   }
 
-  // Assemble booleans to differentiate between states
-  const hasResults = applications.length;
-  const hasSearches = config.searches.length > 0;
-
-  const hasFilter = config.applicationIds.length > 0 || config.regions.length > 0
-    || config.commodities.length > 0 || config.projectTypes.length > 0
-    || config.statuses.length > 0 || config.contentTypes.length > 0;
-
-  const hasDateRange = config.startDate.getTime() !== minDate.getTime()
-    || config.endDate.getTime() !== maxDate.getTime();
-
   return (
     <>
       <Typography variant="h6" className={classes.title}>
@@ -58,7 +43,7 @@ const TreeMapPanel = () => {
       </Typography>
       {
         // Show actual results
-        (hasResults && (
+        (applications.length && (
           <>
             <Typography variant="h6" className={classes.body}>
               {intl.formatMessage({ id: 'components.treeMapPanel.counts' }, {
@@ -72,62 +57,8 @@ const TreeMapPanel = () => {
             <TreeMap />
           </>
         ))
-
-        // Show no results
-        || (!hasSearches && !hasFilter && !hasDateRange && (
-          <Typography variant="h6" style={{ fontWeight: '200' }}>
-            {intl.formatMessage({ id: 'components.treeMapPanel.noResults' })}
-          </Typography>
-        ))
-
-        // Show no results when using keyword search
-        || (hasSearches && !hasFilter && !hasDateRange && (
-          <Typography variant="h6" style={{ fontWeight: '200' }}>
-            {
-              intl.formatMessage({ id: 'components.treeMapPanel.noResultsFor' }, {
-                searches: (<strong style={{ fontWeight: '700' }}>{combinedSearches}</strong>),
-              })
-            }
-          </Typography>
-        ))
-
-        // Show no results when using filters only
-        // (Show this message regardless of date range)
-        || (!hasSearches && hasFilter && (
-          <Typography variant="h6" style={{ fontWeight: '200' }}>
-            {intl.formatMessage({ id: 'components.treeMapPanel.noResultsWithFilters' })}
-          </Typography>
-        ))
-
-        // Show no results when using keyword search and filters
-        // (Show this message regardless of date range)
-        || (hasSearches && hasFilter && (
-          <Typography variant="h6" style={{ fontWeight: '200' }}>
-            {
-              intl.formatMessage({ id: 'components.treeMapPanel.noResultsForWithFilters' }, {
-                searches: (<strong style={{ fontWeight: '700' }}>{combinedSearches}</strong>),
-              })
-            }
-          </Typography>
-        ))
-
-        // Show no results when using keyword search and date range
-        || (hasSearches && !hasFilter && hasDateRange && (
-          <Typography variant="h6" style={{ fontWeight: '200' }}>
-            {
-              intl.formatMessage({ id: 'components.treeMapPanel.noResultsForWithDateRange' }, {
-                searches: (<strong style={{ fontWeight: '700' }}>{combinedSearches}</strong>),
-              })
-            }
-          </Typography>
-        ))
-
-        // Show no results when using date range only
-        || (!hasSearches && !hasFilter && hasDateRange && (
-          <Typography variant="h6" style={{ fontWeight: '200' }}>
-            {intl.formatMessage({ id: 'components.treeMapPanel.noResultsWithDateRange' })}
-          </Typography>
-        ))
+        // Or show no results messages
+        || <NoResultsStatusMessages />
       }
     </>
   );
