6d94549b917752d77c2760e59dd13dd11d7cd95d
Austin Sullivan <austin.sullivan@vizworx.com>
2021-05-11 18:29:58 -0600

    feat(FilterChipsPanel): completed styling of chips; further fleshed out component
    Issues Affected: TASK 1344

--- src/components/FilterChipsPanel/index.jsx
+++ src/components/FilterChipsPanel/index.jsx
@@ -1,31 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Chip, makeStyles } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    borderColor: theme.palette.secondary.main,
-    borderStyle: 'solid',
-    borderWidth: '3px',
-    padding: '1.5em',
+const useStyles = makeStyles(() => ({
+  chip: {
+    background: '#E1F1DA',
+    border: '1px solid black',
+    borderRadius: '30px',
+    margin: '1em 0.5em 0em 0.5em',
+    paddingRight: '0.3em',
+    fontSize: 16,
+    color: 'black',
+  },
+  closeButton: {
+    fill: 'black',
+    transform: 'scale(1.15)',
   },
 }));
 
-const chipData = ['filter1', 'filter2', 'filter3'];
-
-const FilterChipsPanel = () => {
+const FilterChipsPanel = ({ initialState }) => {
   const classes = useStyles();
-  const initialState = [];
+
+  const handleClick = (clickedChip) => () => {
+    console.log("clicked");
+    // TODO: do filter statement to remove given chip name
+  };
 
   return (
     <div>
-      {chipData.map((data) => (
+      {initialState.map((data) => (
         <Chip
           key={data}
           label={data}
+          onClick={handleClick(data)}
+          onDelete={handleClick(data)}
+          deleteIcon={(
+            <CloseIcon
+              className={classes.closeButton}
+            />
+          )}
+          className={classes.chip}
         />
       ))}
     </div>
   );
+};
+
+FilterChipsPanel.defaultProps = {
+  initialState: [],
+};
+
+FilterChipsPanel.propTypes = {
+  initialState: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default FilterChipsPanel;
