69ef349736deaebaab31db6e5ef6bad6c364ca7d
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2021-05-28 18:10:22 -0600

    test: Updated test setup for React Testing Library
    Issues Affected: 2994

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,35 +1,5 @@
-import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import fetch from 'node-fetch';
-
-registerRequireContextHook();
-configure({ adapter: new Adapter() });
-
-/**
- * jest related:
- */
-jest.doMock('file-saver', () => ({ saveAs: jest.fn() }));
-
-/**
- * To get rid of the 'unable to find drag handle' error.
- * https://github.com/atlassian/react-beautiful-dnd/issues/1593
- */
-jest.mock('react-beautiful-dnd', () => ({
-  Droppable: ({ children }) => children({
-    draggableProps: {
-      style: {},
-    },
-    innerRef: jest.fn(),
-  }, {}),
-  Draggable: ({ children }) => children({
-    draggableProps: {
-      style: {},
-    },
-    innerRef: jest.fn(),
-  }, {}),
-  DragDropContext: ({ children }) => children,
-}));
+import '@testing-library/jest-dom';
 
 /**
  * global related:
