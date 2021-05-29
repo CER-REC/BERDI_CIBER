fabe6c243423fa6c6b3b43dfff289c7150a0541b
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2021-05-28 18:06:31 -0600

    refactor: Moved out mock config component for use in testing
    Issues Affected: 2994

--- .storybook/addon-config-and-gql/index.jsx
+++ .storybook/addon-config-and-gql/index.jsx
@@ -1,35 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { makeDecorator } from '@storybook/addons';
 
-import useConfig, { ConfigProvider } from '../../src/hooks/useConfig';
-import { initialState } from '../../src/hooks/reducer';
+import { ConfigProvider } from '../../src/hooks/useConfig';
+import MockConfig from '../../src/tests/MockConfig';
 import client from '../apolloClient';
-
-const MockConfig = ({ state, children }) => {
-  const [isMocked, setIsMocked] = useState(!state);
-  const { config, configDispatch } = useConfig();
-  const hasURLStateLoaded = config !== initialState;
-
-  useEffect(() => {
-    // Need to wait for the URL state to load first due to the bottom up ordering of hook operations
-    if (!hasURLStateLoaded || !state) {
-      return;
-    }
-
-    configDispatch({
-      type: 'changed',
-      payload: state,
-    });
-    setIsMocked(true);
-  }, [hasURLStateLoaded, state, configDispatch, setIsMocked]);
-
-  if (!isMocked) {
-    return 'Mocking config...';
-  }
-
-  return children;
-};
 
 const addConfigAndGQL = (storyFn, context) => (
   <ApolloProvider client={client}>
