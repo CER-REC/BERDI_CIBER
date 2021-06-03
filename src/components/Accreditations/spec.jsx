e63bb90cdf13a5a15d6b8bc82690d241b9894bce
Austin Sullivan <austin.sullivan@vizworx.com>
2021-06-03 17:07:09 -0600

    test(Accreditations/spec): fixed Accreditations to use new test infrastructure

--- src/components/Accreditations/spec.jsx
+++ src/components/Accreditations/spec.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { getRendered, mountWithIntl } from '../../tests/utilities';
+import { render } from '../../tests/utilities';
 import Accreditations from '.';
 
 describe('Components/Accreditations', () => {
   test('should render component', async () => {
-    const wrapper = mountWithIntl(<Accreditations />);
+    const { container } = render(<Accreditations />);
 
-    expect(getRendered(Accreditations, wrapper).exists()).toBeTruthy();
+    expect(container).not.toBeEmpty();
   });
 });
