57e8327b068858b74e1f7a7095538bb45bc39178
Austin Sullivan <austin.sullivan@vizworx.com>
2021-06-28 17:20:28 -0600

    feat(ResultDialog; english.json; french.json): added LoadingIndicator to preview; added load failure
    Issues Affected: TASK 3159

--- src/components/ResultDialog/PDFPreviewer/index.jsx
+++ src/components/ResultDialog/PDFPreviewer/index.jsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import { useIntl } from 'react-intl';
+import { Typography } from '@material-ui/core';
+import LoadingIndicator from '../../LoadingIndicator';
 
 const useStyles = makeStyles(() => ({
   root: {
     width: '93%',
     height: '100%',
     alignSelf: 'center',
-    backgroundColor: '#525659',
+  },
+  loading: {
+    height: 0,
+  },
+  loaded: {
+    height: '100%',
   },
 }));
 
 const PDFPreviewer = ({ pdfURL, pageNumber }) => {
   const classes = useStyles();
+  const intl = useIntl();
+  const [isLoading, setLoading] = useState(true);
+  const handleLoad = () => setLoading(false);
   const fullURL = `${pdfURL}#page=${pageNumber}&pageMode=thumbs`;
+  const className = isLoading ? classes.loading : classes.loaded;
+
   return (
-    <object
-      className={classes.root}
-      data={fullURL}
-      type="application/pdf"
-    >
-      pdf-content
-    </object>
+    <div className={classes.root}>
+      {isLoading && (
+        <div style={{ height: '100%', display: 'flex' }}>
+          <LoadingIndicator type="app" />
+        </div>
+      )}
+      <object
+        className={className}
+        data={fullURL}
+        width="100%"
+        height="100%"
+        type="application/pdf"
+        onLoad={handleLoad}
+      >
+        <Typography>
+          {intl.formatMessage({ id: 'components.resultDialog.failedToLoad' })}
+        </Typography>
+      </object>
+    </div>
   );
 };
 
