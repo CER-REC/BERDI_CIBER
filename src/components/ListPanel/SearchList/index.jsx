1263651142b6cffca05aae58444705a948ca752a
Steven Rothenburger <steven.rothenburger@vizworx.com>
2021-07-06 15:22:09 -0700

    feat(searchlist): implements many of the new design changes for searchlist
    Issues Affected: 3538

--- src/components/ListPanel/SearchList/index.jsx
+++ src/components/ListPanel/SearchList/index.jsx
@@ -9,43 +9,23 @@ import TableRow from '@material-ui/core/TableRow';
 import React, { useMemo, useState } from 'react';
 import { useIntl } from 'react-intl';
 
-import useConfig from '../../hooks/useConfig';
-import useESAData from '../../hooks/useESAData';
-import { reportContent } from '../../utilities/analytics';
-import ResultDialog from '../ResultDialog';
-import PaginationBar from './PaginationBar';
-import PlaceHolderImage from './PlaceHolder.svg';
-import PlusIcon from './PlusIcon.svg';
-import EllipseIcon from './Ellipse.svg';
-import DownCaret from './DownCaret.svg';
-import MagnifyingGlass from './MagnifyingGlass.svg';
+import useConfig from '../../../hooks/useConfig';
+import useESAData from '../../../hooks/useESAData';
+import { reportContent } from '../../../utilities/analytics';
+import ResultDialog from '../../ResultDialog';
+import PaginationBar from '../PaginationBar';
+import PlaceHolderImage from '../../../images/ListPanel/PlaceHolder.svg';
+import EllipseIcon from '../../../images/ListPanel/Ellipse.svg';
+import DownCaret from '../../../images/ListPanel/DownCaret.svg';
+import MagnifyingGlass from '../../../images/ListPanel/MagnifyingGlass.svg';
+import ShelfButton from '../ShelfButton';
 
 const useStyles = makeStyles((theme) => ({
-  imgSection: {
-    textAlign: 'right',
-    '& p': {
-      margin: 'auto',
-      padding: '2px 6px',
-      display: 'block',
-      textTransform: 'uppercase',
-      width: 'max-content',
-      backgroundColor: theme.palette.teal.light,
-      fontWeight: '900',
-      fontSize: '10px',
-      clear: 'right',
-    },
-    '& img': {
-      display: 'block',
-      margin: 'auto',
-      height: '2.5em',
-    },
-    '& button': {
-      display: 'block',
-    },
-  },
   tableHeader: {
-    borderBottom: '1px solid black',
-    padding: '4px',
+    padding: '0.4em 0.4em 0.4em 0',
+    '& p:first-of-type': {
+      marginTop: '1em',
+    },
     '& p': {
       marginTop: '5px',
       borderRadius: '5px',
@@ -55,11 +35,17 @@ const useStyles = makeStyles((theme) => ({
     },
     '& h6': {
       textTransform: 'uppercase',
-      textDecoration: 'underline',
       fontWeight: 'normal',
       color: '#434343',
       display: 'inline',
       cursor: 'pointer',
+    },
+    '& .tableCellInner': {
+      padding: '1em 1em 1em 0',
+      boxShadow: '2px 2px 4px rgba(131,131,131,0.25)',
+      '& img': {
+        cursor: 'pointer',
+      },
     },
   },
   tableParent: {
@@ -76,6 +62,11 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: theme.typography.fontFamily,
     verticalAlign: 'unset',
     paddingLeft: '1em',
+  },
+  viewMoreDetails: {
+    color: theme.palette.blue.dark,
+    fontWeight: 'bold',
+    marginTop: '1em',
   },
 }));
 
@@ -146,57 +137,43 @@ const SearchList = () => {
   return (
     <>
       <ResultDialog open={open} onClose={handleClose} data={selectedLineData} />
-
       <TableContainer component={Paper} className={classes.tableParent}>
         <Table className={classes.table} aria-label="custom pagination table">
           <TableBody>
             {contents.map((content) => (
               <TableRow key={content.id}>
-
-                <TableCell component="th" scope="row" style={{ border: 'none' }} className={classes.tableHeader}>
-
-                  <Grid container direction="row" style={{ padding: '1em', boxShadow: '2px 2px 4px rgba(131,131,131,0.25)' }}>
-                    <Grid item xs={2}>
+                <TableCell component="th" scope="row" className={classes.tableHeader}>
+                  <Grid className="tableCellInner" container>
+                    <Grid item xs={2} onClick={() => handleClickOpen(content)}>
                       <img alt="A generic table" src={PlaceHolderImage} style={{ width: '14em' }} />
                       <img alt="A magnifying glass" src={MagnifyingGlass} style={{ transform: 'translate(110px, -50px)' }} />
                     </Grid>
+
                     <Grid item xs={8}>
                       {createTitleSection(content.title, content)}
                       <Typography variant="body2">
                         <span>{intl.formatMessage({ id: 'common.fullProjectName' })}</span>
                         {content.application.name}
                       </Typography>
+
                       <Typography variant="body2">
                         <span>{intl.formatMessage({ id: 'common.companyName' })}</span>
-                        Placeholder
+                        {/* TODO: this data needs to come in from the application query */}
+                        Placeholder Lorem ipsum dolor sit.
                       </Typography>
 
-                      <ButtonBase
-                        className={classes.seeMoreButton}
-                        style={{ paddingLeft: 0, marginTop: '1em' }}
-                        onClick={() => {}}
-                      >
+                      <ButtonBase className={classes.viewMoreDetails}>
                         View more details...
                         <img alt="Down caret" src={DownCaret} />
-
                       </ButtonBase>
                     </Grid>
+
                     <Grid container item direction="column" alignItems="flex-end" justify="space-around" xs={2}>
-                      <Grid container item direction="row" justify="space-between" alignItems="center" style={{ backgroundColor: '#D2EDEB', borderRadius: '5px', width: '100%', padding: '0 1em ' }}>
-
-                        <Grid item>
-                          <img alt="A plus icon" src={PlusIcon} style={{}} />
-                        </Grid>
-                        <Grid item>
-                          <p>
-                            Add to shelf
-                          </p>
-                        </Grid>
-
-                      </Grid>
-
-                      <Grid item>
-                        <img alt="Ellipse" src={EllipseIcon} />
+                      <ShelfButton />
+                      <Grid item style={{ paddingRight: '1em' }}>
+                        <ButtonBase>
+                          <img alt="Ellipse" src={EllipseIcon} />
+                        </ButtonBase>
                       </Grid>
                     </Grid>
                   </Grid>
---
+++ src/components/ListPanel/ShelfButton/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Grid, makeStyles, ButtonBase } from '@material-ui/core';
+import PlusIcon from '../../../images/ListPanel/ShelfButton/PlusIcon.svg';
+
+const useStyles = makeStyles(() => (
+  {
+    root: {
+      backgroundColor: '#D2EDEB',
+      borderRadius: '5px',
+      width: '100%',
+      padding: '0.7em 1em ',
+    },
+  }
+));
+
+const ShelfButton = () => {
+  const classes = useStyles();
+  return (
+    <ButtonBase className={classes.root}>
+      <Grid container item direction="row" justify="space-between" alignItems="center">
+
+        <Grid item>
+          <img alt="A plus icon" src={PlusIcon} />
+        </Grid>
+
+        <Grid item>
+          Add to shelf
+        </Grid>
+      </Grid>
+    </ButtonBase>
+  );
+};
+
+export default ShelfButton;
