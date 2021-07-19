850535a5705b0c3e4a42831a4c6a8f558f46308a
Austin Sullivan <austin.sullivan@vizworx.com>
2021-07-19 14:54:13 -0600

    feat(ResultDialog/PDFPreviewer): rendering pdf load failure image
    Issues Affected: TASK 3891

--- src/components/ResultDialog/PDFPreviewer/index.jsx
+++ src/components/ResultDialog/PDFPreviewer/index.jsx
@@ -3,17 +3,26 @@ import { makeStyles, Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
 import LoadingIndicator from '../../LoadingIndicator';
+import pdfFailLoadIcon from '../../../images/pdfFailLoad.svg';
 
 const useStyles = makeStyles(() => ({
   root: {
-    width: '94%',
+    width: '100%',
     height: '100%',
     alignSelf: 'center',
     overflow: 'hidden',
+    padding: '0 2.25em 0 2.25em',
+  },
+  object: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 }));
 
 const PDFPreviewer = ({ pdfURL, pageNumber }) => {
+  pdfURL = 'force failure';
   const classes = useStyles();
   const intl = useIntl();
   const [isLoading, setLoading] = useState(true);
@@ -32,11 +41,18 @@ const PDFPreviewer = ({ pdfURL, pageNumber }) => {
         width="100%"
         height="100%"
         type="application/pdf"
+        typemustmatch="true"
+        className={classes.object}
         onLoad={handleLoad}
       >
-        <Typography>
-          {intl.formatMessage({ id: 'components.resultDialog.failedToLoad' })}
-        </Typography>
+        {!isLoading && (
+          <>
+            <img alt="Icon depicting pdf load failure." src={pdfFailLoadIcon} />
+            <Typography>
+              {intl.formatMessage({ id: 'components.resultDialog.failedToLoad' })}
+            </Typography>
+          </>
+        )}
       </object>
     </div>
   );
