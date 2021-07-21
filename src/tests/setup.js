05821cd96cbc1d6500acecd6af9dfd4220cc073b
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2021-07-21 12:50:55 -0600

    feat(tests): Testing setup and configuration for rendering entire app
    Issues Affected: 3655

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -11,6 +11,16 @@ global.fetch = fetch;
 global.open = jest.fn();
 global.scrollTo = jest.fn();
 
+// Responsive nivo charts rely on the getBoundingClientRect sizes to render
+HTMLElement.prototype.getBoundingClientRect = () => ({
+  width: 100,
+  height: 100,
+  top: 0,
+  left: 0,
+});
+
+HTMLElement.prototype.scrollIntoView = jest.fn();
+
 // Define is needed to override the location assign function references
 Object.defineProperty(global, 'location', {
   value: {
@@ -18,3 +28,5 @@ Object.defineProperty(global, 'location', {
     assign: jest.fn(),
   },
 });
+
+jest.spyOn(global.history, 'pushState');
