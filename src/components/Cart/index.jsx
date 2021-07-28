0522f2f7bbeebc9ad4b357e2c97d3c878e95bda5
Austin Sullivan <austin.sullivan@vizworx.com>
2021-07-28 16:38:13 -0600

    feat(Cart; pages/Search): added Cart to Search page; added button to show Cart; other progress
    Issues Affected: TASK 3658

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Drawer, Grid, makeStyles } from '@material-ui/core';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { Button, Drawer, Grid, IconButton, makeStyles } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 
 // TODO: will we need utilities/analytics here?
 
 const useStyles = makeStyles(() => ({
-  root: {
+  button: {
+    backgroundColor: '#D2EDEB',
+    position: 'fixed',
+    right: 0,
+    top: '50%',
+  },
+  drawer: {
     width: '30em',
     height: '92%',
     flexGrow: 1,
@@ -13,44 +19,68 @@ const useStyles = makeStyles(() => ({
   header: {
     minHeight: '10%',
     backgroundColor: '#D2EDEB',
-    borderBottom: '8px solid #66C8C3'
+    borderBottom: '8px solid #66C8C3',
+  },
+  closeButton: {
+    textAlign: 'right',
   },
   body: {
     minHeight: '65%',
     borderBottom: '8px solid #66C8C3',
-    overflowY: 'scroll',
+    overflowY: 'auto',
   },
   footer: {
     minHeight: '25%',
-  }
+  },
 }));
 
-const Cart = ({ open, onClose }) => {
+const Cart = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
-    <Drawer
-      anchor="right"
-      open={open}
-      onClose={onClose}
-      classes={{ paper: classes.root }}
-    >
-      <Grid container className={classes.header}>
-        header
-      </Grid>
-      <Grid container className={classes.body}>
-        body
-      </Grid>
-      <Grid container className={classes.footer}>
-        footer
-      </Grid>
-    </Drawer>
+    (!open && (
+      <Button className={classes.button} onClick={handleOpen} variant="contained">
+        <Grid container justifyContent="space-between" alignItems="center">
+          {/* TODO: show shelf icon */}
+          <Grid item xs={6}>
+            icon
+          </Grid>
+          {/* TODO: show cart item count label */}
+          <Grid item xs={6}>
+            label
+          </Grid>
+        </Grid>
+      </Button>
+    ))
+    || (
+      <Drawer
+        anchor="right"
+        open={open}
+        classes={{ paper: classes.drawer }}
+        BackdropProps={{ invisible: true }}
+      >
+        <Grid container alignItems="center" justifyContent="space-between" className={classes.header}>
+          <Grid item xs={6} />
+          <Grid item xs={3} />
+          <Grid item xs={3} className={classes.closeButton}>
+            <IconButton aria-label="close" onClick={handleClose}>
+              <CloseIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Grid container className={classes.body}>
+          body
+        </Grid>
+        <Grid container className={classes.footer}>
+          footer
+          {/* TODO: show disclaimer and download button */}
+        </Grid>
+      </Drawer>
+    )
   );
-};
-
-Cart.propTypes = {
-  open: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
 };
 
 export default Cart;
