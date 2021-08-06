c1b7c120393983bc107524ace76720eb9de09892
Austin Sullivan <austin.sullivan@vizworx.com>
2021-08-06 17:41:14 -0600

    fix(fileSizeFormatter): readded undefined and null check
    Issues Affected: TASK 3658

--- src/utilities/fileSizeFormatter.js
+++ src/utilities/fileSizeFormatter.js
@@ -1,4 +1,5 @@
 export default (fileSize) => {
+  if (fileSize === undefined || fileSize === null) return null;
   const fileSizeMB = fileSize / 1024;
   return (fileSizeMB >= 1) ? `${fileSizeMB.toFixed(2)} MB` : `${fileSize.toFixed(2)} KB`;
 };
