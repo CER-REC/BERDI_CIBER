cd83bf49771413de0d85fd23bb12cf6d18d8abc3
Austin Sullivan <austin.sullivan@vizworx.com>
2021-08-06 17:28:14 -0600

    fix(fileSizeFormatter): removed line so 0 is used
    Issues Affected: TASK 3658

--- src/utilities/fileSizeFormatter.js
+++ src/utilities/fileSizeFormatter.js
@@ -1,5 +1,4 @@
 export default (fileSize) => {
-  if (!fileSize) return null;
   const fileSizeMB = fileSize / 1024;
   return (fileSizeMB >= 1) ? `${fileSizeMB.toFixed(2)} MB` : `${fileSize.toFixed(2)} KB`;
 };
