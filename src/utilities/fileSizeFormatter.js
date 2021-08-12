3961d22c2af6442fadce7ee3a14e176010bea150
Austin Sullivan <austin.sullivan@vizworx.com>
2021-08-12 12:41:23 -0600

    refactor(fileSizeFormatter): condensed logic
    Issues Affected: TASK 3658

--- src/utilities/fileSizeFormatter.js
+++ src/utilities/fileSizeFormatter.js
@@ -1,5 +1,5 @@
 export default (fileSize) => {
-  if (fileSize === undefined || fileSize === null) return null;
+  if (typeof fileSize !== 'number') return null;
   const fileSizeMB = fileSize / 1024;
   return (fileSizeMB >= 1) ? `${fileSizeMB.toFixed(2)} MB` : `${fileSize.toFixed(2)} KB`;
 };
