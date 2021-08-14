c2dc5b89ec5a052e51f3ef13dc451f7319413be0
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2021-08-13 21:06:33 -0600

    feat: Added topic filter to the landing page
    Issues Affected: 3089

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -4,6 +4,7 @@ import BetaAlert from '../../components/BetaAlert';
 import NavButtons from '../../components/NavButtons';
 import SearchPanel from '../../components/SearchPanel';
 import TitleCard from '../../components/TitleCard';
+import TopicsFilter from '../../components/TopicsFilter';
 import Disclaimer from './Disclaimer';
 import Discoveries from './Discoveries';
 
@@ -12,6 +13,7 @@ const Landing = () => (
     <BetaAlert />
     <TitleCard />
     <SearchPanel hasFilter={false} />
+    <TopicsFilter />
     <Disclaimer />
     <Discoveries />
     <NavButtons />
