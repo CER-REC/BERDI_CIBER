b61c9daccf9b3d9fc9fecd5bbb33914a6793121d
Austin Sullivan <austin.sullivan@vizworx.com>
2021-08-16 14:33:56 -0600

    feat(Cart): extracted styles from Cart into its own styles file; removed justifyContent prop
    Issues Affected: TASK 3665

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -7,79 +7,9 @@ import shelfIcon from '../../images/cart/shelf.svg';
 import useDownloadSize from '../../hooks/useDownloadSize';
 import useConfig from '../../hooks/useConfig';
 import fileSizeFormatter from '../../utilities/fileSizeFormatter';
+import styles from './styles';
 
-const useStyles = makeStyles((theme) => ({
-  cartButton: {
-    backgroundColor: theme.palette.cart.light,
-    position: 'fixed',
-    right: 0,
-    top: '50%',
-    zIndex: '1',
-    padding: '10px 10px 5px 0',
-    borderRadius: 0,
-  },
-  newDot: {
-    fill: '#D32645',
-    strokeWidth: 0,
-    position: 'relative',
-    left: '40px',
-    bottom: '4px',
-  },
-  newDotHidden: {
-    visibility: 'hidden',
-  },
-  cartButtonLabel: {
-    color: theme.palette.cart.dark,
-    fontSize: '13px',
-    fontWeight: '900',
-    margin: '0 5px 0 5px',
-    paddingLeft: '5px',
-  },
-  drawer: {
-    width: '28em',
-  },
-  closeButton: {
-    textAlign: 'right',
-  },
-  header: {
-    backgroundColor: theme.palette.cart.light,
-    borderBottom: '8px solid',
-    borderBottomColor: theme.palette.secondary.main,
-  },
-  body: {
-    borderBottom: '8px solid',
-    borderBottomColor: theme.palette.secondary.main,
-    overflowY: 'auto',
-    height: '100%',
-  },
-  footerDisabled: {
-    opacity: 0.25,
-    pointerEvents: 'none',
-  },
-  footer: {
-    opacity: 1,
-    pointerEvents: 'auto',
-  },
-  footerDisclaimer: {
-    backgroundColor: '#F7F7FB',
-    margin: '1em',
-    padding: '1em',
-    textAlign: 'center',
-  },
-  footerDisclaimerText: {
-    fontSize: '13px',
-  },
-  footerDownloadButton: {
-    marginBottom: '2em',
-    backgroundColor: theme.palette.cart.dark,
-    padding: '0.3em 3em',
-  },
-  footerDownloadButtonIcon: {
-    overflow: 'visible',
-    paddingRight: '0.5em',
-    maxWidth: '1.2em',
-  },
-}));
+const useStyles = makeStyles(styles);
 
 const Cart = () => {
   const classes = useStyles();
@@ -120,7 +50,7 @@ const Cart = () => {
         >
           <circle cx={newDotR} cy={newDotR} r={newDotR} />
         </svg>
-        <Grid container alignItems="center" spacing={1} justifyContent="flex-start">
+        <Grid container alignItems="center" spacing={1}>
           <Grid item xs={6}>
             <Icon style={{ overflow: 'visible' }}>
               <img src={shelfIcon} alt="a shelf holding books" style={{ maxWidth: '1.6em' }} />
---
+++ src/components/Cart/styles.jsx
@@ -0,0 +1,72 @@
+export default (theme) => ({
+  cartButton: {
+    backgroundColor: theme.palette.cart.light,
+    position: 'fixed',
+    right: 0,
+    top: '50%',
+    zIndex: '1',
+    padding: '10px 10px 5px 0',
+    borderRadius: 0,
+  },
+  newDot: {
+    fill: '#D32645',
+    strokeWidth: 0,
+    position: 'relative',
+    left: '40px',
+    bottom: '4px',
+  },
+  newDotHidden: {
+    visibility: 'hidden',
+  },
+  cartButtonLabel: {
+    color: theme.palette.cart.dark,
+    fontSize: '13px',
+    fontWeight: '900',
+    margin: '0 5px 0 5px',
+    paddingLeft: '5px',
+  },
+  drawer: {
+    width: '28em',
+  },
+  closeButton: {
+    textAlign: 'right',
+  },
+  header: {
+    backgroundColor: theme.palette.cart.light,
+    borderBottom: '8px solid',
+    borderBottomColor: theme.palette.secondary.main,
+  },
+  body: {
+    borderBottom: '8px solid',
+    borderBottomColor: theme.palette.secondary.main,
+    overflowY: 'auto',
+    height: '100%',
+  },
+  footerDisabled: {
+    opacity: 0.25,
+    pointerEvents: 'none',
+  },
+  footer: {
+    opacity: 1,
+    pointerEvents: 'auto',
+  },
+  footerDisclaimer: {
+    backgroundColor: '#F7F7FB',
+    margin: '1em',
+    padding: '1em',
+    textAlign: 'center',
+  },
+  footerDisclaimerText: {
+    fontSize: '13px',
+  },
+  footerDownloadButton: {
+    marginBottom: '2em',
+    backgroundColor: theme.palette.cart.dark,
+    padding: '0.3em 3em',
+  },
+  footerDownloadButtonIcon: {
+    overflow: 'visible',
+    paddingRight: '0.5em',
+    maxWidth: '1.2em',
+  },
+});
