214807568d0334c9f537c8b4ee60f160e3dfbc6b
Austin Sullivan <austin.sullivan@vizworx.com>
2021-08-16 11:27:06 -0600

    feat(fileSizeFormatter): download size label now localized
    Issues Affected: TASK 4072

--- src/utilities/fileSizeFormatter.js
+++ src/utilities/fileSizeFormatter.js
@@ -1,5 +1,11 @@
+import { useIntl } from 'react-intl';
+
 export default (fileSize) => {
   if (typeof fileSize !== 'number') return null;
+
+  const intl = useIntl();
   const fileSizeMB = fileSize / 1024;
-  return (fileSizeMB >= 1) ? `${fileSizeMB.toFixed(2)} MB` : `${fileSize.toFixed(2)} KB`;
+  return (fileSizeMB >= 1)
+    ? `${Number(fileSizeMB.toFixed(2)).toLocaleString(intl.locale)} MB`
+    : `${Number(fileSize.toFixed(2)).toLocaleString(intl.locale)} KB`;
 };
