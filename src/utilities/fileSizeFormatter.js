b561d515fde4d7190a4e7ed10c6247b4800401a6
Austin Sullivan <austin.sullivan@vizworx.com>
2021-08-18 16:33:42 -0600

    fix(fileSizeFormatter): fixed hook issue

--- src/utilities/fileSizeFormatter.js
+++ src/utilities/fileSizeFormatter.js
@@ -1,9 +1,8 @@
 import { useIntl } from 'react-intl';
 
 export default (fileSize) => {
-  if (typeof fileSize !== 'number') return null;
-
   const intl = useIntl();
+  if (typeof fileSize !== 'number') return null;
   const fileSizeMB = fileSize / 1024;
   return (fileSizeMB >= 1)
     ? `${Number(fileSizeMB.toFixed(2)).toLocaleString(intl.locale)} MB`
