3ce015975dc7271dcf4d56d14c8ff12f9a22f68b
Steven Rothenburger <steven.rothenburger@vizworx.com>
2021-09-08 14:15:46 -0700

    feat(searchDetails): moves counts from treemap to searchDetails
    Issues Affected: 4228

--- src/components/SearchDetails/index.jsx
+++ src/components/SearchDetails/index.jsx
@@ -1,13 +1,72 @@
+import { Grid, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
-import { Grid } from '@material-ui/core';
-
+import { useIntl } from 'react-intl';
+import useConfig from '../../hooks/useConfig';
+import useESAData from '../../hooks/useESAData';
 import FilterToggle from './FilterToggle';
 
-// TODO: Refactor search result overview from TreeMapPanel into this component
-const SearchDetails = () => (
-  <Grid container style={{ padding: '2em 0' }}>
-    <FilterToggle />
-  </Grid>
-);
+const useStyles = makeStyles({
+  counts: {
+    fontWeight: 'normal',
+  },
+  bold: {
+    fontWeight: '700',
+  },
+  filterText: {
+    fontWeight: '400',
+    paddingBottom: '0.5em',
+    paddingTop: '0.8em',
+  },
+});
+
+const SearchDetails = () => {
+  const classes = useStyles();
+  const intl = useIntl();
+  const { applications, loading } = useESAData();
+  const { config: { filter } } = useConfig();
+
+  const figureCount = applications.reduce(
+    (count, application) => (application.figureCount + count),
+    0,
+  );
+
+  const tableCount = applications.reduce(
+    (count, application) => (application.tableCount + count),
+    0,
+  );
+
+  if (!applications.length && loading) {
+    return null;
+  }
+
+  return (
+    <div style={{ paddingLeft: '0.3em' }}>
+      <Grid container justify="space-between" style={{ padding: '1em 0 0.5em' }}>
+        <Typography variant="body2" className={classes.bold}>
+          {intl.formatMessage({ id: 'components.searchDetails.title' }).toUpperCase()}
+        </Typography>
+        <Typography variant="body2" className={classes.bold}>
+          {intl.formatMessage({ id: 'components.searchDetails.explore' }).toUpperCase()}
+        </Typography>
+      </Grid>
+      <Grid container justify="space-between">
+        <Typography variant="h6" className={classes.counts}>
+          {intl.formatMessage({ id: 'components.searchDetails.counts' }, {
+            tables: tableCount,
+            figures: figureCount,
+            boldTables: (<span className={classes.bold}>{tableCount}</span>),
+            boldFigures: (<span className={classes.bold}>{figureCount}</span>),
+          })}
+        </Typography>
+        <FilterToggle />
+      </Grid>
+      {filter === 'topic' && (
+      <Typography variant="h6" className={classes.filterText}>
+          {intl.formatMessage({ id: 'components.searchDetails.select' })}
+      </Typography>
+      )}
+    </div>
+  );
+};
 
 export default SearchDetails;
