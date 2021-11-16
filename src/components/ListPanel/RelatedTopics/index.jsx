99ed83d3d28a6d966e6eefd7e6d139b5a76d6f90
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2021-11-16 15:39:51 -0700

    feat(ListPanel/RelatedTopics): Added gender prop for no topic French message
    Issues Affected: 3164

--- src/components/ListPanel/RelatedTopics/index.jsx
+++ src/components/ListPanel/RelatedTopics/index.jsx
@@ -6,6 +6,8 @@ import RelatedTopicsDialog from '../RelatedTopicsDialog';
 import getScore from '../../../utilities/getScore';
 import { socioEconomicTopics } from '../../../constants';
 
+const maleTypes = ['ALIGNMENT_SHEET'];
+
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: '#F4F4F4',
@@ -120,6 +122,7 @@ const RelatedTopics = ({ valueComponents, type }) => {
           <Typography variant="h6" className={classes.title}>
             {intl.formatMessage({ id: 'components.listPanel.relatedTopics.noTopics' }, {
               type: intl.formatMessage({ id: `api.content.${type}` }).toLowerCase(),
+              gender: maleTypes.includes(type) ? 'male' : 'female',
             })}
           </Typography>
       )}
