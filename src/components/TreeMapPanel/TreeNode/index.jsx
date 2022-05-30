828a8353815341f67e88c1a676f0030bdeacc763
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2022-05-30 16:00:07 -0600

    feat(TreeMapPanel): Added keyboard accessibility and a timer to reduce the number of onClick callbacks
    Issues Affected: 8852

--- src/components/TreeMapPanel/TreeNode/index.jsx
+++ src/components/TreeMapPanel/TreeNode/index.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { darken, makeStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { useIntl } from 'react-intl';
+import isActivateKey from '../../../utilities/isActivateKey';
 
-const useStyles = makeStyles({
+const noop = () => { };
+const debounceMS = 20;
+
+const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexFlow: 'column wrap',
     height: '100%',
     width: '100%',
+    '&:focus-visible': {
+      backgroundColor: darken(theme.palette.teal.dark, 0.2),
+      outline: 'none',
+    },
   },
   block: {
     padding: '14px 0 0 14px',
@@ -30,14 +38,31 @@ const useStyles = makeStyles({
   },
   emptyPlaceholder: { width: '100%' },
   title: { fontSize: '130%' },
-});
+}));
 
 const TreeNode = ({ title, figureCount, tableCount, alignmentSheetCount }) => {
   const intl = useIntl();
   const classes = useStyles();
+  const [debounceId, setDebounceId] = useState();
+  const handleKeyPress = (event) => {
+    if (isActivateKey(event)) {
+      const target = event.currentTarget;
+      const timeoutId = setTimeout(() => target.click(), debounceMS);
 
+      clearTimeout(debounceId);
+      setDebounceId(timeoutId);
+    }
+  };
+
+  // Let click events bubble up to Nivo's node containers
   return (
-    <div className={`TreeNode ${classes.root}`}>
+    <div
+      role="button"
+      className={`TreeNode ${classes.root}`}
+      tabIndex="0"
+      onClick={noop}
+      onKeyPress={handleKeyPress}
+    >
       <div className={classes.emptyPlaceholder} />
       <div className={classes.block}>
         <span className={classes.title}>{title}</span>
