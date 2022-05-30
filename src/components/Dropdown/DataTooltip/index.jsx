3ad84e6fbe3a492a748f5f129813ccf8b8af6081
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2022-05-30 14:50:32 -0600

    feat(Dropdown/DataTooltip): Changed project type help components to add keyboard accessibility
    Issues Affected: 8852

--- src/components/Dropdown/DataTooltip/index.jsx
+++ src/components/Dropdown/DataTooltip/index.jsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import { ClickAwayListener, Tooltip, Typography, makeStyles } from '@material-ui/core';
+import React, { useState } from 'react';
+import { IconButton, Popover, Typography, makeStyles } from '@material-ui/core';
 import { HelpOutline } from '@material-ui/icons';
 import { useIntl } from 'react-intl';
 
@@ -7,18 +7,22 @@ const useStyles = makeStyles((theme) => ({
   root: {
     color: '#5D5D5D',
     fontSize: 16,
-    height: '1.2em',
     marginLeft: '0.5em',
     marginBottom: '0.2em',
-    verticalAlign: 'bottom',
+    padding: 0,
+  },
+  icon: {
+    fontSize: 'inherit',
+    height: '1.2em',
     width: '1.2em',
-    cursor: 'pointer',
   },
   tooltip: {
     border: '1px solid #BBBBBB',
     boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
     borderRadius: 5,
-    padding: '2em',
+    padding: '1.25em',
+    marginLeft: '0.88em',
+    maxWidth: '18.75em',
   },
   body: {
     color: theme.palette.primary.main,
@@ -28,56 +32,56 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const DataTooltip = () => {
-  const [open, setOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const classes = useStyles();
   const intl = useIntl();
-  const handleClose = useCallback(() => setOpen(false), [setOpen]);
-  const handleOpen = useCallback(() => setOpen(true), [setOpen]);
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
 
   return (
-    <ClickAwayListener onClickAway={handleClose}>
-      <Tooltip
-        title={(
-          <div className={classes.tooltip}>
-            <Typography classes={{ root: classes.body }}>
-              {
-                intl.formatMessage(
-                  { id: 'components.dropdown.tooltip' },
-                  {
-                    nebAct: (
-                      <a
-                        href={intl.formatMessage({ id: 'components.dropdown.nebLink' })}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {intl.formatMessage({ id: 'components.dropdown.nebAct' })}
-                      </a>
-                    ),
-                    cerAct: (
-                      <a
-                        href={intl.formatMessage({ id: 'components.dropdown.cerLink' })}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        {intl.formatMessage({ id: 'components.dropdown.cerAct' })}
-                      </a>
-                    ),
-                  },
-                )
-              }
-            </Typography>
-          </div>
-        )}
-        open={open}
-        placement="right-start"
-        disableFocusListener
-        disableHoverListener
-        disableTouchListener
-        interactive
+    <>
+      <IconButton classes={{ root: classes.root }} onClick={handleClick}>
+        <HelpOutline className={classes.icon} />
+      </IconButton>
+      <Popover
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        PaperProps={{ classes: { root: classes.tooltip } }}
       >
-        <HelpOutline className={`DataTooltip ${classes.root}`} onClick={handleOpen} />
-      </Tooltip>
-    </ClickAwayListener>
+        <Typography classes={{ root: classes.body }}>
+          {
+            intl.formatMessage(
+              { id: 'components.dropdown.tooltip' },
+              {
+                nebAct: (
+                  <a
+                    href={intl.formatMessage({ id: 'components.dropdown.nebLink' })}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {intl.formatMessage({ id: 'components.dropdown.nebAct' })}
+                  </a>
+                ),
+                cerAct: (
+                  <a
+                    href={intl.formatMessage({ id: 'components.dropdown.cerLink' })}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {intl.formatMessage({ id: 'components.dropdown.cerAct' })}
+                  </a>
+                ),
+              },
+            )
+          }
+        </Typography>
+      </Popover>
+    </>
   );
 };
 
