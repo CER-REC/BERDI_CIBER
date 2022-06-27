07ea0e4ce6db4a984d06da291dc57c8aefd7e5b0
Dianna Yim <dianna.yim@vizworx.com>
2022-06-27 16:38:13 -0600

    feat(Components/Dropdown/DataTooltip): updated tooltip text
    Tooltip text has been updated to reflect the new text. No French translation has been added yet.
    
    Issues Affected: 9449

--- src/components/Dropdown/DataTooltip/index.jsx
+++ src/components/Dropdown/DataTooltip/index.jsx
@@ -1,33 +1,33 @@
-import React, { useState } from 'react';
-import { IconButton, Popover, Typography, makeStyles } from '@material-ui/core';
-import { HelpOutline } from '@material-ui/icons';
-import { useIntl } from 'react-intl';
+import React, { useState } from "react";
+import { IconButton, Popover, Typography, makeStyles } from "@material-ui/core";
+import { HelpOutline } from "@material-ui/icons";
+import { useIntl } from "react-intl";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    color: '#5D5D5D',
+    color: "#5D5D5D",
     fontSize: 16,
-    marginLeft: '0.5em',
-    marginBottom: '0.2em',
+    marginLeft: "0.5em",
+    marginBottom: "0.2em",
     padding: 0,
   },
   icon: {
-    fontSize: 'inherit',
-    height: '1.2em',
-    width: '1.2em',
+    fontSize: "inherit",
+    height: "1.2em",
+    width: "1.2em",
   },
   tooltip: {
-    border: '1px solid #BBBBBB',
-    boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
+    border: "1px solid #BBBBBB",
+    boxShadow: "2px 2px 4px rgba(0, 0, 0, 0.1)",
     borderRadius: 5,
-    padding: '1.25em',
-    marginLeft: '0.88em',
-    maxWidth: '18.75em',
+    padding: "1.25em",
+    marginLeft: "0.88em",
+    maxWidth: "18.75em",
   },
   body: {
     color: theme.palette.primary.main,
     fontSize: 14,
-    lineHeight: 'normal',
+    lineHeight: "normal",
   },
 }));
 
@@ -48,38 +48,57 @@ const DataTooltip = () => {
         anchorEl={anchorEl}
         onClose={handleClose}
         anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
+          vertical: "top",
+          horizontal: "right",
         }}
         PaperProps={{ classes: { root: classes.tooltip } }}
       >
         <Typography classes={{ root: classes.body }}>
-          {
-            intl.formatMessage(
-              { id: 'components.dropdown.tooltip' },
-              {
-                nebAct: (
-                  <a
-                    href={intl.formatMessage({ id: 'components.dropdown.nebLink' })}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {intl.formatMessage({ id: 'components.dropdown.nebAct' })}
-                  </a>
-                ),
-                cerAct: (
-                  <a
-                    href={intl.formatMessage({ id: 'components.dropdown.cerLink' })}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {intl.formatMessage({ id: 'components.dropdown.cerAct' })}
-                  </a>
-                ),
-              },
-            )
-          }
+          {intl.formatMessage(
+            { id: "components.dropdown.tooltip" },
+            {
+              cerAct: (
+                <a
+                  href={intl.formatMessage({
+                    id: "components.dropdown.cerLink",
+                  })}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {intl.formatMessage({ id: "components.dropdown.cerAct" })}
+                </a>
+              ),
+              nebAct: (
+                <a
+                  href={intl.formatMessage({
+                    id: "components.dropdown.nebLink",
+                  })}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {intl.formatMessage({ id: "components.dropdown.nebAct" })}
+                </a>
+              ),
+            }
+          )}
         </Typography>
+        <ul>
+          <li>
+            <Typography classes={{ root: classes.body }}>
+              {intl.formatMessage({ id: "components.dropdown.bullet1" })}
+            </Typography>
+          </li>
+          <li>
+            <Typography classes={{ root: classes.body }}>
+              {intl.formatMessage({ id: "components.dropdown.bullet2" })}
+            </Typography>
+          </li>
+          <li>
+            <Typography classes={{ root: classes.body }}>
+              {intl.formatMessage({ id: "components.dropdown.bullet3" })}
+            </Typography>
+          </li>
+        </ul>
       </Popover>
     </>
   );
