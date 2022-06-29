c38d01d81e872bc704246fd7be157315896ca491
Dianna Yim <dianna.yim@vizworx.com>
2022-06-28 20:25:39 -0600

    feat(SearchDetails): moved search details and changed explore results
    Search details was moved to under the search panel. Explore results was made larger.
    
    Issues Affected: 9458

--- src/components/SearchDetails/index.jsx
+++ src/components/SearchDetails/index.jsx
@@ -1,23 +1,26 @@
 import { Grid, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import { useIntl } from 'react-intl';
-import useConfig from '../../hooks/useConfig';
 import useESAData from '../../hooks/useESAData';
-import FilterToggle from './FilterToggle';
 import NoResultsStatusMessages from './NoResultsStatusMessages';
 import { lang } from '../../constants';
 
 const useStyles = makeStyles({
-  counts: {
+  normalFontWeight: {
     fontWeight: 'normal',
+    fontSize: '20px',
   },
   bold: {
     fontWeight: '700',
+    fontSize: '23px',
   },
   filterText: {
     fontWeight: '400',
     paddingBottom: '0.5em',
     paddingTop: '0.8em',
+  },
+  smallerFontSize: {
+    fontSize: '20px',
   },
 });
 
@@ -25,7 +28,6 @@ const SearchDetails = () => {
   const classes = useStyles();
   const intl = useIntl();
   const { applications, loading } = useESAData();
-  const { config: { filter } } = useConfig();
 
   const figureCount = applications.reduce(
     (count, application) => (application.figureCount + count),
@@ -45,11 +47,8 @@ const SearchDetails = () => {
   return (
     <div style={{ paddingLeft: '0.3em' }}>
       <Grid container justify="space-between" style={{ padding: '1em 0 0.5em' }}>
-        <Typography variant="body2" className={classes.bold}>
+        <Typography variant="body2">
           {intl.formatMessage({ id: 'components.searchDetails.title' }).toUpperCase()}
-        </Typography>
-        <Typography variant="body2" className={classes.bold}>
-          {intl.formatMessage({ id: 'components.searchDetails.explore' }).toUpperCase()}
         </Typography>
       </Grid>
 
@@ -58,7 +57,7 @@ const SearchDetails = () => {
           !loading && (
             (applications.length && (
               <div>
-                <Typography variant="h6" className={classes.counts}>
+                <Typography variant="h6" className={classes.normalFontWeight}>
                   {intl.formatMessage({ id: 'components.searchDetails.counts' }, {
                     tables: tableCount,
                     figures: figureCount,
@@ -74,14 +73,7 @@ const SearchDetails = () => {
             )) || <NoResultsStatusMessages />
           )
         )}
-        <FilterToggle />
       </Grid>
-
-      {filter === 'topic' && (
-        <Typography variant="h6" className={classes.filterText}>
-          {intl.formatMessage({ id: 'components.searchDetails.select' })}
-        </Typography>
-      )}
     </div>
   );
 };
