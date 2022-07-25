be1222d2a906b8817f71e070600b3eb024d7e8d4
Dianna Yim <dianna.yim@vizworx.com>
2022-07-25 14:05:28 -0600

    fix(SearchActionResults): fixed linting
    Issues Affected: 9573

--- src/components/SearchActionResults/index.jsx
+++ src/components/SearchActionResults/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Grid } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { useIntl } from 'react-intl';
@@ -23,7 +23,7 @@ const useStyles = makeStyles(() => ({
     width: '100%',
     borderTop: '1px dotted',
     margin: '0',
-    marginTop: topMargin
+    marginTop: topMargin,
   },
 }));
 
@@ -89,7 +89,7 @@ const SearchActionResults = ({ content }) => {
       >
         <img alt="a generic flag" src={flag} />
         {intl.formatMessage({
-          id: 'components.searchActionResults.report'
+          id: 'components.searchActionResults.report',
         })}
       </Button>
     </>
