85507cff89dbd9f797c2fcda6bad230b6fbb0c68
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2022-10-28 12:09:06 -0600

    fix(Dropdown): Sort in numerical descending order for results in Firefox

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -81,14 +81,13 @@ const DropDown = ({ type, hasHelp, options, value, onChange }) => {
       ));
     }
 
-    const sorted = options.map((entry) => (
-      { key: entry, value: getDropdownItemName(entry) })).sort((a, b) => {
-      if (type === 'resultCount') {
-        return a.key > b.key;
-      }
+    const sorted = options.map((entry) => ({
+      key: entry,
+      value: getDropdownItemName(entry)
+    })).sort((a, b) => {
       const x = a.value.toLowerCase();
       const y = b.value.toLowerCase();
-      return x.localeCompare(y);
+      return x.localeCompare(y, undefined, { numeric: true });
     });
 
     return sorted;
