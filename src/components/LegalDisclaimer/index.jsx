aa131b050ec285f89fab7220e34d9a13cf987dd7
aclchen@ucalgary.ca <aclchen@ucalgary.ca>
2022-12-16 15:32:23 -0700

    feat(LegalDisclaimer): Updated legal disclaimer to design
    Issues Affected: 11151, 11152

--- src/components/LegalDisclaimer/index.jsx
+++ src/components/LegalDisclaimer/index.jsx
@@ -1,9 +1,10 @@
-import {
-  Button, Checkbox, Dialog, FormControl, FormControlLabel, Grid, makeStyles, Typography,
-} from '@material-ui/core';
-import React, { useState } from 'react';
+import { Dialog, Grid, Typography, makeStyles } from '@material-ui/core';
+import React from 'react';
 import { useIntl } from 'react-intl';
 import PropTypes from 'prop-types';
+import DownloadFooter from './DownloadFooter';
+import Footer from './Footer';
+import Citation from '../Citation';
 import { lang } from '../../constants';
 import cerLogoEn from '../../images/cerLogoEn.png';
 import cerLogoFr from '../../images/cerLogoFr.png';
@@ -20,43 +21,41 @@ const useStyles = makeStyles((theme) => ({
     '& .MuiGrid-container': {
       color: theme.palette.grey.dark,
     },
-  },
-  paper: {
-    maxWidth: '610px',
-    padding: '2em',
-  },
-  checked: {
-    '&$checked': {
-      color: theme.palette.blue.dark,
+    '& button': {
+      padding: '0.2em 1.5em',
     },
   },
-  unChecked: {
-    color: 'grey',
+  title: {
+    color: 'black',
+    padding: '1em 0',
+    textAlign: 'center',
+  },
+  paper: {
+    maxWidth: '41em',
+    padding: '2em',
+  },
+  body: {
+    '& > :last-child': { paddingTop: '1em' },
   },
 }));
 
-const LegalDisclaimer = ({ onClose, open, setOpen }) => {
+const LegalDisclaimer = ({ title, url, open, onClose }) => {
   const classes = useStyles();
   const intl = useIntl();
-  const [checked, setChecked] = useState(false);
   const logo = lang === 'fr' ? cerLogoFr : cerLogoEn;
-
-  const handleClose = (_, reason) => { if (reason !== 'backdropClick') { onClose(); setOpen(false); setChecked(false); } };
-  const toggleChecked = () => setChecked(!checked);
 
   return (
     <Dialog
       open={open}
-      onClose={handleClose}
       PaperProps={{ className: classes.paper }}
       disableEscapeKeyDown
       className={classes.root}
     >
       <img alt={intl.formatMessage({ id: 'components.legalDisclaimer.logoAltText' })} src={logo} style={{ maxWidth: '23em' }} />
-      <Grid container justify="center">
+      <Grid className={classes.body} container justify="center">
         <div>
-          <Typography variant="h6" style={{ padding: '1.5em 0', color: 'black' }}>
-            {intl.formatMessage({ id: 'components.legalDisclaimer.attention' })}
+          <Typography className={classes.title} variant="h6">
+            { title }
           </Typography>
           <Typography style={{ paddingBottom: '1em' }}>
             {intl.formatMessage({ id: 'components.legalDisclaimer.disclaimer1' })}
@@ -79,35 +78,22 @@ const LegalDisclaimer = ({ onClose, open, setOpen }) => {
             </li>
           </ul>
         </div>
-        <Grid container justify="flex-end" alignItems="center" style={{ paddingTop: '1em' }}>
-          <FormControl>
-            <FormControlLabel
-              value="agreed"
-              control={<Checkbox className={checked ? classes.checked : classes.unChecked} />}
-              label={intl.formatMessage({ id: 'components.legalDisclaimer.agree' })}
-              labelPlacement="end"
-              onChange={toggleChecked}
-            />
-          </FormControl>
-          <Button
-            disabled={!checked}
-            variant="contained"
-            color="primary"
-            style={{ padding: '0.2em 1.5em' }}
-            onClick={handleClose}
-          >
-            {intl.formatMessage({ id: 'components.legalDisclaimer.enter' })}
-          </Button>
-        </Grid>
+        <Citation />
+        { url ? <DownloadFooter url={url} onClick={onClose} /> : <Footer onClick={onClose} /> }
       </Grid>
     </Dialog>
   );
 };
 
-export default LegalDisclaimer;
-
 LegalDisclaimer.propTypes = {
-  onClose: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
-  setOpen: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
 };
+
+LegalDisclaimer.defaultProps = {
+  url: '',
+};
+
+export default LegalDisclaimer;
